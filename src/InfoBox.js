8880356678776f246130ef6fbdcec87b7708682c
DJ <social.deej@gmail.com>
2020-11-11 01:37:00 +0530

    adding cores to modules

---
+++ src/InfoBox.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Card, CardContent, Typography} from "@material-ui/core"
+
+function InfoBox({title, cases, total}) {
+    return (
+        <Card>
+            <CardContent>
+                {/* Title ie Coronavirus Cases */}
+
+                {/* Number of Cases */}
+
+                {/* 1.2M total */}
+            </CardContent>
+        </Card>
+    )
+}
+
+export default InfoBox
