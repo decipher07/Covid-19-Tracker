0983a7d88741ad747c72bcc484d509b98a182175
DJ <social.deej@gmail.com>
2020-11-11 00:00:51 +0530

    Adding structure Schema

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import {MenuItem, FormControl, Select, Card, CardContent, } from "@material-ui/core"
 
 function App() {
   return (
     <div className="app">
       <h1>Covid 19 Tracker </h1>
+      
+      <FormControl className="app__dropdown">
+        <Select variant="outlined" value="abc">
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Option 2</MenuItem>
+          <MenuItem value="worldwide">Option 3</MenuItem>
+        </Select>
+      </FormControl>
+
+      {/* Header */}
+      {/* Title + Select Input Dropdown field */}
+
+      {/* InfoBox */}
+      {/* InfoBox */}
+      {/* InfoBox */}
+      
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   );
 }
