e54017906642f4f440611f8e2bf2b5325a6c8a28
DJ <social.deej@gmail.com>
2020-11-11 10:20:59 +0530

    The map changing values

--- src/Table.js
+++ src/Table.js
@@ -4,7 +4,10 @@ function Table({countries}) {
     return (
         <div className="table">
             {countries.map(country => (
-                
+                <tr>
+                    <td></td>
+                    <td></td>
+                </tr>
             ))}
         </div>
     )
