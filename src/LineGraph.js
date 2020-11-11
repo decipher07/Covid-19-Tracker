d5418b3624f4fe9369d91e118e44db05ddb39ed1
DJ <social.deej@gmail.com>
2020-11-11 22:15:20 +0530

    Adding Graph content apis

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,11 +1,11 @@
-import React, { useState} from 'react'
+import React, { useState , useEffect} from 'react'
 import {Line} from 'react-chartjs-2'
 
 function LineGraph() {
     const [data, setData] = useState({})
     //https://disease.sh/v3/covid-19/historical/all?lastdays=120
-
-
+    
+    
     return (
         <div>
             <Line data options/>
