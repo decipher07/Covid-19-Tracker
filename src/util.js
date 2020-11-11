5173accb32dc0a10db71925fef3eeebd43b57f47
DJ <social.deej@gmail.com>
2020-11-11 10:40:26 +0530

    using sorting from utils

---
+++ src/util.js
@@ -0,0 +1,9 @@
+export const sortData = (data) => {
+    const sortedData = [...data];
+    sortedData.sort((a,b) => {
+        if (a.cases > b.cases)
+            return -1 ;
+        return 1;
+    })
+    return sortedData;
+}
