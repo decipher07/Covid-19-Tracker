3a6aeac9b0f09c0644fa1e65ef4565776967c1d7
DJ <social.deej@gmail.com>
2020-11-11 23:19:02 +0530

    Check Code Documentation

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,37 +1,97 @@
 import React, { useState , useEffect} from 'react'
 import {Line} from 'react-chartjs-2'
 
+const options = {
+    legend: {
+      display: false,
+    },
+    elements: {
+      point: {
+        radius: 0,
+      },
+    },
+    maintainAspectRatio: false,
+    tooltips: {
+      mode: "index",
+      intersect: false,
+      callbacks: {
+        label: function (tooltipItem, data) {
+          return numeral(tooltipItem.value).format("+0,0");
+        },
+      },
+    },
+    scales: {
+      xAxes: [
+        {
+          type: "time",
+          time: {
+            format: "MM/DD/YY",
+            tooltipFormat: "ll",
+          },
+        },
+      ],
+      yAxes: [
+        {
+          gridLines: {
+            display: false,
+          },
+          ticks: {
+            // Include a dollar sign in the ticks
+            callback: function (value, index, values) {
+              return numeral(value).format("0a");
+            },
+          },
+        },
+      ],
+    },
+};
+
+const buildChartData = (data, casesType='cases') => {
+    const chartData = [];
+    let lastDataPoint;
+    for(let date in data.cases) {
+        if (lastDataPoint) {
+            const newDataPoint = {
+                x: data,
+                y: data[casesType][date] - lastDataPoint
+            }
+            chartData.push(newDataPoint);
+        }
+        lastDataPoint = data[casesType][date];
+    }
+    return chartData;
+}
+
 function LineGraph() {
     const [data, setData] = useState({})
     //https://disease.sh/v3/covid-19/historical/all?lastdays=120
     useEffect(() => {
-        fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data)
-        })
-    }, [])
+        const fetchData = async () => {
+            await fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+                const chartData = buildChartData(data);
+                setData(chartData)
+            })
+        }
 
-    const buildChartData = (data, casesType='cases') => {
-        const chartData = [];
-        let lastDataPoint;
-        data[casesType].forEach(date => {
-            if (lastDataPoint) {
-                const newDataPoint = {
-                    x: data,
-                    y: data[casesType][date] - lastDataPoint
-                }
-                chartData.push(newDataPoint);
-            }
-            lastDataPoint = data[casesType][date];
-        })
-        return chartData;
-    }
+        fetchData();
+
+    }, [])
 
     return (
         <div>
-            <h1>I am a import from 'react-apollo' </h1>
-            {/* <Line data options/> */}
+            <h1>I am a graph </h1>
+            <Line data = {{
+                datasets : [{
+                    backgroundColor: "rgba(204, 16, 52, 0.5)",
+                    borderColor: "#CC1034",
+                    data:data
+                }]
+            }} 
+            options={options}
+            />
         </div>
     )
 }
