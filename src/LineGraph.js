a1f6a67b8580dc0b22f758b448a0411b359b9afd
DJ <social.deej@gmail.com>
2020-11-11 22:26:18 +0530

    Adding furthur more dependencies

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -4,11 +4,18 @@ import {Line} from 'react-chartjs-2'
 function LineGraph() {
     const [data, setData] = useState({})
     //https://disease.sh/v3/covid-19/historical/all?lastdays=120
-    
-    
+    useEffect(() => {
+        fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+        .then((response) => response.json())
+        .then((data) => {
+
+        })
+    })
+
     return (
         <div>
-            <Line data options/>
+            <h1>I am a import { compose, graphql } from 'react-apollo' </h1>
+            {/* <Line data options/> */}
         </div>
     )
 }
