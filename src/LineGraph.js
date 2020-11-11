d6247ca4058ef78fb28e0aaf8f7efa36bc2f7a72
DJ <social.deej@gmail.com>
2020-11-11 22:04:18 +0530

    adding LineGraph

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState} from 'react'
+import {Line} from 'react-chartjs-2'
 
 function LineGraph() {
+    const [data, setData] = useState({})
+    //https://disease.sh/v3/covid-19/historical/all?lastdays=120
+
+
     return (
         <div>
-            
+            <Line data options/>
         </div>
     )
 }
