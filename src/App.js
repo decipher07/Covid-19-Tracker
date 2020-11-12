54e68b4eac2e01f9aff128377ed9efea62b66735
DJ <social.deej@gmail.com>
2020-11-12 18:06:09 +0530

    Map Working Successfully

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ function App() {
   const [country, setCountry] = useState('worldwide')
   const [countryInfo, setCountryInfo] = useState({})
   const [tableData, setTableData] = useState([])
+  const [mapCenter, setMapCenter] = useState({lat: 34.80746, lng: -48.4796 })
+  const [mapZoom, setMapZoom] = useState(3);
 
   useEffect(() => {
     fetch('https://disease.sh/v3/covid-19/all')
