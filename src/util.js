7e86ae620ded3dfe12d9937cebb254818bc52bc3
DJ <social.deej@gmail.com>
2020-11-12 18:51:00 +0530

    Added Casesand Recovery Pops

--- src/util.js
+++ src/util.js
@@ -33,7 +33,13 @@ export const showDataonMap = (data, casesType='cases') => (
     data.map((country) => (
         <Circle center={[country.countryInfo.lat, country.countryInfo.long]} color={casesTypeColors[casesType].hex} fillColor={casesTypeColors[casesType].hex} fillOpacity={0.4} radius={ Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier/2 }>
             <Popup>
-                <h1>I'm a Pop up</h1>
+                <div>
+                    <div style={{ backgroundImage: `url(${country.countryInfo.flag})`}}/>
+                    <div>{country.country}</div>
+                    <div>Cases: {numeral(country.cases).format("0,0")}</div>
+                    <div>Recovered: {numeral(country.recovered).format("0,0")}</div>
+                    <div>Deaths: {numeral(country.deaths).format("0,0")}</div>
+                </div>
             </Popup>
         </Circle>
     ))
